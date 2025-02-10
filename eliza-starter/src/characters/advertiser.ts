b550c394d8ac9955592e8556b1147bb4ffad2045
Dru1DD <mrtproger@gmail.com>
2025-02-10 12:23:41 +0100

    feat: add plugin sui

--- eliza-starter/src/characters/advertiser.ts
+++ eliza-starter/src/characters/advertiser.ts
@@ -6,7 +6,6 @@ import {
 } from "@elizaos/core";
 import { imageGenerationPlugin } from "../plugin/imagePlugin/index.ts";
 import { bootstrapPlugin } from "@elizaos/plugin-bootstrap";
-import baseSepPlugin from "../plugin/baseSepPlugin/src/index.ts";
 export const ADVERTISER_AGENT_ID = "58c9913b-a8ff-4cff-87d9-fbdb1b25ff34";
 
 export const advertiser: Character = {
@@ -16,7 +15,7 @@ export const advertiser: Character = {
   clients: [Clients.TWITTER],
   modelProvider: ModelProviderName.OPENROUTER,
   imageModelProvider: ModelProviderName.TOGETHER,
-  plugins: [imageGenerationPlugin, bootstrapPlugin, baseSepPlugin],
+  plugins: [imageGenerationPlugin, bootstrapPlugin],
   settings: {
     voice: {
       model: "en_US-male-medium",
