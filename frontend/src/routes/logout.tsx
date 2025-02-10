cd2e95664e90ed7ee0722b8788a8d6eb03413078
Dru1DD <mrtproger@gmail.com>
2025-02-10 17:24:44 +0100

    feat: add wallet support

--- frontend/src/routes/logout.tsx
+++ frontend/src/routes/logout.tsx
@@ -1,20 +1,20 @@
-import { useDisconnect } from 'wagmi';
 import { useNavigate } from 'react-router';
 import useLogoutMutation from '@/hooks/mutations/useLogoutMutation';
 import useAsyncEffect from '@/hooks/useAsyncEffect';
 import LoadingScreen from '@/components/LoadingScreen';
+import { useDisconnectWallet } from '@mysten/dapp-kit';
 
 const Logout = () => {
-  const { disconnectAsync } = useDisconnect();
+  const { mutate: disconnect } = useDisconnectWallet();
   const { mutateAsync: logout } = useLogoutMutation();
   const navigate = useNavigate();
 
   useAsyncEffect(async () => {
-    await disconnectAsync();
+    await disconnect();
     await logout(undefined);
 
     navigate('/');
-  }, [disconnectAsync, logout]);
+  }, [disconnect, logout]);
 
   return <LoadingScreen />;
 };
